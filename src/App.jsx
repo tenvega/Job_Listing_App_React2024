27502e9627f07b1ba0fa334f13c535ecaa236836
tenvega <etorra@gmail.com>
2024-05-05 16:38:37 -0400

    Added Hero, HomeCards and Card Components, Also created jobsjson file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import { HomeCards } from "./components/HomeCards"
 
 
 const App = () => {
@@ -9,41 +10,9 @@ const App = () => {
 
   <Navbar />
   <Hero />
-  
-{/*<!--Developers and Employers-->*/}
-   <section className="py-4">
-      <div className="container-xl lg:container m-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Developers</h2>
-            <p className="mt-2 mb-4">
-              Browse our React jobs and start your career today
-            </p>
-            <a
-              href="/jobs.html"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Jobs
-            </a>
-          </div>
-          <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Employers</h2>
-            <p className="mt-2 mb-4">
-              List your job to find the perfect developer for the role
-            </p>
-            <a
-              href="/add-job.html"
-              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
-            >
-              Add Job
-            </a>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    
-    <section className="bg-blue-50 px-4 py-10">
+  <HomeCards />
+   
+   <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           Browse Jobs
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
+/* eslint-disable react/prop-types */
 
-
-const Hero = () => {
+const Hero = ({title="Become a React Dev", subtitle="Find the React job that fits your skill set"}) => {
   return (
      <section className="bg-indigo-700 py-20 mb-4">
       <div
@@ -10,10 +10,10 @@ const Hero = () => {
           <h1
             className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
           >
-            Become a React Dev
+            {title}
           </h1>
           <p className="my-4 text-xl text-white">
-            Find the React job that fits your skills and needs
+            {subtitle}
           </p>
         </div>
       </div>
